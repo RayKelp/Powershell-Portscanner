170d532df7dfd9d230c86db3bd89b1dd104d415f
Rayan <wingsneon@gmail.com>
2023-09-01 09:40:29 -0400

    added script

---
+++ ps-portscan.ps1
@@ -0,0 +1,60 @@
+Write-Host ""
+Write-Host " Insert an IP or a list of IP addresses you'd like to scan. Separe them by space (Ex: 192.168.1.1 192.168.1.2 192.168.1.3)"
+Write-Host " Then insert the ports you want you check, also separated by space."
+Write-Host ""
+$ipInput = (Read-Host "IP(s)")
+$ports = (Read-Host "Porta(s)") -Split ' '
+
+# If CIDR, then calculate the number of hosts
+if ($ipInput -match '/')
+{
+	$ip, $cidr = $ipInput -split '/'
+	# Calcular quantidade de hosts baseado no cidr	
+	$subnetSize = [math]::Pow(2, (32 - [int]$cidr)) - 2 # Subtrai 2 hosts - endereco da rede e endereço de breadcast
+
+	# Monta lista com todos os hosts da sub-rede
+	$ips = 1..$subnetSize | Foreach-Object{ # A quantidade de IPs  será de 1 até o valor de subnetSize
+		$ipArray = $ip.Split('.')
+		$lastByte = [int]$ipArray[3] + $_
+		$ipArray[3] = $lastByte
+		$ipArray -join '.'
+	}
+	clear
+	Write-Host "Scanning ""$subnetSize"" hosts in the IP ""$ip"" for the following ports: ""$ports"""
+}
+# If not CIDR, just separe the ips by spaces.
+else
+{
+	$ips = $ipInput -Split ' '
+	clear
+	Write-Host "Scanning ""$ips"" for the ports ""$ports"""
+}
+
+# Start scan
+Write-Host "------------------------------------------------------"
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+
+foreach ($ip in $ips)
+{
+	Write-Host "$ip`:"
+	foreach ($p in $ports)
+	{
+		$result = Test-NetConnection -ComputerName $ip -Port $p -WarningAction SilentlyContinue -InformationLevel Quiet
+		
+		if($result -eq "True") {Write-Host "      $p - OK"}
+
+		else{Write-Host "      $p - Fail"}
+    
+	}
+    Write-Host ""
+}
+
+Write-Host ""
+Write-Host ""
+Write-host "Done."
+Write-Host ""
+pause
